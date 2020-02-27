40f764b43ce91e45b9bc8edfd9be24449a09c361
Evan Lih <evanlih@uw.edu>
2020-02-27 11:07:01 -0800

    Testing plot

--- ReadingSHapeFile.R
+++ ReadingSHapeFile.R
@@ -16,9 +16,18 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # 
 # ggplot() + geom_polygon(data = test, aes(x = long, y = lat, group = group, fill = COUNTRY))
 
+testImport <- read_xlsx("Final Data (1).xlsx")
+
+testImport <- rename(testImport, COUNTRY = Country)
+
 mapLink="https://github.com/EvanLih/PUBPOL-543-BEES-Project/raw/master/UIA_World_Countries_Boundaries.json"
 
 PROJmap="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 worldMap=topojson_read(mapLink,crs=PROJmap,stringsAsFactors = FALSE)
 
-ggplot(data=worldMap) + geom_sf()
+testMerge <- merge.sf(testImport, worldMap, by = "COUNTRY", all.x = F)
+st_geometry(testMerge) <- testMerge$geometry
+
+
+ggplot(data=worldMap) + geom_sf() +
+  geom_sf(data = testMerge, aes(fill=`Sustainable Development Index`),color=NA,show.legend = T)
